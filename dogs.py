5bbef2244ceddebe7449132b2d09f3f338af882d
ArmanKarapetyan1998 <Chevrolet_Lanos178@outlook.com>
2024-09-08 11:50:57 +0500

    Создаём главное окно проекта,и метку кнопку Вроде сделали

--- dogs.py
+++ dogs.py
@@ -1,7 +1,34 @@
 from tkinter import *
+from tkinter import messagebox
 import requests
 from PIL import Image,ImageTk
 from io import BytesIO
+
+def get_dog_image():
+    try:
+        response=requests.get('https://dog.ceo/api/breeds/image/random')
+        response.raise_for_status()
+        data=response.json()
+        return data('message')
+    except Exception as e:
+        messagebox.showerror('Ошибка', f'Возникла ошибка при загрузке api {e}')
+
+
+def show_image():
+    image_url=get_dog_image()
+    if image_url:
+        try:
+            response=requests.get(image_url,stream=True)
+            response.raise_for_status()
+            img_data=BytesIO(response.content)
+            img=Image.open(img_data)
+            img.thumbnail((300,300))
+            img=ImageTk.PhotoImage(img)
+            label.config(image=img)
+            label.image=img
+        except Exception as e:
+            messagebox.showerror('Ошибка',f'Возникла ошибка при загрузке изображения {e}')
+            return None
 
 
 window=Tk
